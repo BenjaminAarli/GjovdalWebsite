ad53213c58f423ea94c589a16d2ade54d6533434
PandaUnwise <32360561+BenjaminAarli@users.noreply.github.com>
2026-02-28 04:04:30 +0100

    Update ArtistDiv.svelte

--- src/lib/ArtistDiv.svelte
+++ src/lib/ArtistDiv.svelte
@@ -1,10 +1,10 @@
-<div class="ArtistDivaaa">
+<div class="ArtistDiv">
     <h1>Arne og Ynge</h1>
     <p>Fredag, Lørdag og Søndag</p>
 </div>
 
 <style>
-    .ArtistDivaaa {
+    .ArtistDiv {
         position: relative;
         width: 250px;
         height: 250px;
