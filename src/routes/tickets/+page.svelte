e00d9e0dbc39222768244c244da6f157a3ba5f4f
PandaUnwise <32360561+BenjaminAarli@users.noreply.github.com>
2026-04-01 12:31:15 +0200

    Change the Map layer button.
    Made it prettier to look at.
    and move the buy button to after the map.

--- src/routes/tickets/+page.svelte
+++ src/routes/tickets/+page.svelte
@@ -24,14 +24,9 @@
     };
 </script>
 
-
-<div class="buy_div {are_tickets_selected ? 'tickets' : ''}">
-    <button on:click={() => {move_to_sale_page()}} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 round">
-        Kjøp nå!
-    </button>
+<div style="justify-content: center; width: 100%; display: flex; margin: 16px;">
+    <button class="MapChangeButton" on:click={() => {map_layer = !map_layer}}>Klick her for å see kart</button>
 </div>
-
-<button on:click={() => {map_layer = !map_layer}}>Map Layer</button>
 <div class="BGImageDiv">
     {#if map_layer}
     <img src="/badmap_03_nogrid_02.png" alt="Map">
@@ -40,16 +35,32 @@
         <NumberGrid style="position: absolute; left: 255px; top: 38px;  transform: scale(0.75, 0.75);   grid-template-rows: repeat(7, 1fr);" grid_amount={28} bind:tickets_selected={grid_b_selected} on:tickets_selected={() => {update_tickets(); console.log(tickets_selected)}} starting_id={1} />
         <NumberGrid style="position: absolute; left: 140px; top: 20px;  transform: scale(0.75, 0.7);    grid-template-rows: repeat(6, 1fr);" grid_amount={12} bind:tickets_selected={grid_b_selected} on:tickets_selected={() => {update_tickets(); console.log(tickets_selected)}} starting_id={13} />
         <NumberGrid style="position: absolute; left: 200px; top: 55px;  transform: scale(0.75);         grid-template-rows: repeat(2, 1fr);" grid_amount={2}  bind:tickets_selected={grid_b_selected} on:tickets_selected={() => {update_tickets(); console.log(tickets_selected)}} starting_id={1} /> 
-    
-        <NumberGrid style="position: absolute; left: 25px;  top: 190px; width: 200px; height: 75px; transform: scale(0.75, 0.75);    grid-template-rows: repeat(3, 1fr);" grid_amount={15}  bind:tickets_selected={grid_b_selected} on:tickets_selected={() => {update_tickets(); console.log(tickets_selected)}} starting_id={1} /> 
         
+        <NumberGrid style="position: absolute; left: 25px;  top: 190px; width: 200px; height: 75px; transform: scale(0.75, 0.75);    grid-template-rows: repeat(3, 1fr);" grid_amount={15}  bind:tickets_selected={grid_b_selected} on:tickets_selected={() => {update_tickets(); console.log(tickets_selected)}} starting_id={1} /> 
     </div>
     {:else}
         <iframe title="Overlay Map" src="https://www.google.com/maps/d/embed?mid=1jqO1f3EsvO3zMc1XXGKe1frrifqFhdk&ehbc=2E312F" style="width: 100%; height: 100%;"></iframe>
     {/if}
 </div>
-        
+                    
+<div class="buy_div {are_tickets_selected ? 'tickets' : ''}">
+    <button on:click={() => {move_to_sale_page()}} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 round">
+        Kjøp nå!
+    </button>
+</div>
+
 <style>
+    .MapChangeButton {
+        position: relative;
+        width: 200px;
+        height: 60px;
+        
+        margin: auto;
+
+        border: 1px solid grey;
+        border-radius: 4px;
+        background-color: whitesmoke;
+    }
     .BGImageDiv {
         position: relative;
         margin: auto;
@@ -57,7 +68,7 @@
         height: 500px;
         border: 4px solid black;
         border-radius: 8px;
-        margin-bottom: 1000px;
+
     }
     .BGImageDiv img {
         position: absolute;
