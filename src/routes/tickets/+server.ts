460ffcfb94399c21eed2767cd62678af45eb490f
PandaUnwise <32360561+BenjaminAarli@users.noreply.github.com>
2026-04-03 08:02:16 +0200

    updated tickets server.ts
    Made it use getDatabase() instead of db directly.

--- src/routes/tickets/+server.ts
+++ src/routes/tickets/+server.ts
@@ -1,5 +1,5 @@
 import { json } from "@sveltejs/kit";
-import { db } from "$lib/server/db";
+import { getDatabase } from "$lib/server/db";
 import { ticket } from "$lib/server/db/schema";
 import type { RequestHandler } from "./$types";
 
@@ -7,7 +7,7 @@ export const POST: RequestHandler = async ({ request }) => {
     const data = await request.json();
 
     const { email, quantity } = data;
-    await db.insert(ticket).values({
+    await getDatabase().insert(ticket).values({
         buy_date: 20250221, 
         ticket_days: 1,
         price: 300,
@@ -16,9 +16,4 @@ export const POST: RequestHandler = async ({ request }) => {
 
     return new Response(JSON.stringify({ success: true }));
 };
-
-// The days are integers. 
-// Friday is 1, Saturday 2, Sunday 3. 
-// Friday and Saturday is 4, Friday and Sunday is 5, 
-// Saturday and Sunday is 6, 
-// Friday, Saturday and Sunday is 7. 
+ 
