acaf3144c2634ffbfac500f1049a17656011fd8a
PandaUnwise <32360561+BenjaminAarli@users.noreply.github.com>
2026-04-03 07:35:35 +0200

    Fixed a major issue in index.ts
    There was an issue where index.ts tried to trigger the database on every load attempt so I fixed it so that it only collects data from the database on request. Big mistake, my bad.

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,13 +2,11 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
 import 'dotenv/config';
-
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-
-const client = postgres(process.env.DATABASE_URL!);
-
-// export const db = drizzle(client, { schema });
-
 import { env } from '$env/dynamic/private';
 
-export const db = drizzle(env.DATABASE_URL);
+export function getDatabase() {
+    if (!env.DATABASE_URL) {
+        throw new Error('DATABASE_URL is not set.');
+    }
+    return drizzle(env.DATABASE_URL)
+}
